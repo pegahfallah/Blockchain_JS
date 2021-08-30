525b63c8060b9304b2a6ed2f589becbca1ee4ff2
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-08-30 11:56:05 -0400

    transaction constructor

--- blockchain.js
+++ blockchain.js
@@ -1,15 +1,26 @@
 var sha256 = require("js-sha256");
 var secp256k1 = require("secp256k1");
+const { randomBytes } = require("crypto");
+// generate message to sign
+// message should have 32-byte length, if you have some other length you can hash message
+// for example `msg = sha256(rawMessage)`
+// const msg = randomBytes(32)
+// // verify the signature
+// console.log(secp256k1.ecdsaVerify(sigObj.signature, msg, pubKey))
+// => true
 class Transaction {
   //utxo -->
-  constructor(input, prevOutput, signature) {
-    //might need to pass an amount
-    this.id = ""; //hash of body
+  constructor(prevOutput, publicKey) {
+    // val: get the amount from the previous output
+    this.publicKey = publicKey;
     this.prevOutput = prevOutput;
-    this.input = this.signPreviousOutput(); //signed previous output
-    this.output = this.generateOutput();
-    this.signature = signature;
-    this.privKet;
+    this.privKey;
+    // header of transaction
+    this.id = this.hashTranscationBody();
+    // body of transaction
+    this.input = prevOutput;
+    this.outputs = this.generateOutput();
+    this.signature = this.signPrevOutput();
   }
 
   hashTransactionBody = () => {
@@ -18,48 +29,54 @@ class Transaction {
     return sha256.hmac(sha256.hmac(this.input, this.outputs), this.signature); //this wont work
     //return sha256.hmac({...this.outputs, this.input}, this.signature)
   };
-
-  signPreviousOutput = () => {
-    //out input
-    let privKey;
+  signPrevOutput = () => {
+    // generate privKey
+    // let privKey
     do {
-      this.privKet = randomBytes(32);
+      this.privKey = randomBytes(32);
     } while (!secp256k1.privateKeyVerify(this.privKey));
-    {
-      const signedPrevOutput = secp256k1.ecdsaSign(
-        this.prevOutput,
-        this.privKey
-      );
-    }
+    // sign the previous output
+    return secp256k1.ecdsaSign(this.prevOutput, this.privKey);
   };
 
   generateOutput = () => {
-    const pubKey = secp256k1.publicKeyCreate(this.privKey);
-    return [pubKey, amount];
+    // get the public key in a compressed format
+    // const pubKey = secp256k1.publicKeyCreate(this.privKey)
+    return [this.publicKey, this.prevOutput.amount];
+  };
+
+  // Note: Remember the seed transaction!
+
+  isTransactionValid = () => {
+    // check The transaction id is a valid hash over the transaction body
+    // The public key associated to the unspent transaction output is derived from a Secp256k1 private key
+    // The public key associated to the unspent transaction output used as the input, successfully validates the signature
+    //  The value of ARB associated with the input is greater than or equal to the sum of the values associated with the outputs
+  };
+
+  hasSeedTransaction = () => {
+    // check that transaction includes seed
   };
 }
 
 class Block {
-  constructor(prevHash, transactionList) {
+  constructor(index, prevHash, transactionList) {
+    this.index = index;
     this.prevHash = prevHash;
-    this.transactionList = transactionList; //transaction list = [seedTransaction, ....]
-    this.nonce = calculateNonce();
+    this.transactionList = transactionList; // transactionList = [seedTrasaction, ....]
+    this.nonce = this.calculateNonce();
   }
 
-  //SHA256 HASH OF PREVIOUS BLOCK
-  //returns a hash
   calculateNonce = () => {
-    return sha256.hmax(this.prevHash, this.transactionList);
+    // returns a hash
+    return sha256.hmac(this.prevHash, this.transactionList);
   };
 
-  //Mining
-  //return a boolean
-
+  // Mining
   validateBlock = () => {
+    // returns a boolean
     if (this.prevHash && this.transactionList && this.nonce) {
-      //transaction list isValid() returns
-      ///or transactionList.hasSeed() returns
-      //check that transaction includes the seed isValid && hasSeed() returns true
+      // so transaction.isTransactionValid() && transaction.hasSeedTrasanction() ? return true: else false
     }
   };
 }
@@ -69,9 +86,9 @@ class BlockChain {
     this.chain = [];
   }
 
-  createGenesisBlock = () => {
-    return new Block(0, 0, "Genesis List");
-  };
+  // createGenesisBlock = () => {
+  //   return new Block(0, 0, "Genesis List");
+  // };
 
   getRecentBlock = () => {
     return this.chain[this.chain.length - 1];
@@ -90,13 +107,30 @@ class BlockChain {
   };
 
   isChainValid = () => {
-    //2 pointers
     for (let i = 0; i < this.chain.length; i++) {
       const currBlock = this.chain[i];
       const prevBlock = this.chain[i - 1];
+
+      if (currBlock.nonce != currBlock.calculateNonce()) {
+        return false;
+      }
+
+      if (currBlock.prevHash != prevBlock.nonce) {
+        return false;
+      }
+
+      return true;
     }
   };
 }
 
 // let block = new Block("", "")
 // let ourBlockchain = new Blockchain();
+
+// let senderPrivKey =
+// let senderPublicKey =
+// let transaction0 = ([senderPublicKey, 10])
+// let block0 = new Block(0, 0, transaction0 )
+// let ourBlockchain = new BlockChain();
+// ourBlockchain.addBlock(block0)
+// let block1 = new Block(1, block0.nonce, newTransaction)
